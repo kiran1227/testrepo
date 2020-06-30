56e23dbda98c52ad9662fd1f8be8df03475ab15e
kiran <thekiran12@gmail.com>
2020-06-30 15:10:00 +0545

    first commit

---
+++ database/migrations/2020_06_29_180930_create_contact_infos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact_infos', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->integer('phone')->nullable();
+            $table->string('address')->nullable()->default('itahari');
+            $table->string('message', 300);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contact_infos');
+    }
+}
